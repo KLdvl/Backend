8e91e0430d9d2c7d7591a6b343eed52aedbd0595
Kevin <maskevin27@gmail.com>
2022-07-27 13:11:35 +0000

    modified message

--- middleware/authentification.js
+++ middleware/authentification.js
@@ -14,6 +14,6 @@ module.exports = async (req, res, next) => {
       next();
     }
   } catch (error) {
-    res.status(401).json({ error: error | "Requête non authentifiée" });
+    res.status(401).json({ "Requête non authentifiée" });
   }
 };
