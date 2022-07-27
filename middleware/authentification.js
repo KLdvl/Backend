f35b7b1a311ba59368b85e5bdecb3e0c56e708d0
Kevin <maskevin27@gmail.com>
2022-07-27 13:34:13 +0000

    console logs

--- middleware/authentification.js
+++ middleware/authentification.js
@@ -5,8 +5,12 @@ const jwt = require("jsonwebtoken");
 module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
+    console.log(token)
     const decodedToken = jwt.verify(token, process.env.JWT_TOKEN);
+    console.log(decodedToken)
     const userId = decodedToken.userId;
+    console.log(userId)
+    console.log(req.auth)
     req.auth = { userId };
     if (req.body.userId && req.body.userId !== userId) {
       throw "User ID non valide";
